8d851b4dc4788e29b18ceff60a8bdbd486d8eda6
phanngan811 <s3863893@rmit.edu.vn>
2022-09-03 20:46:10 +0700

    finish function comment

--- coddiFront-end/src/services/comment.service.js
+++ coddiFront-end/src/services/comment.service.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 const API_URL = "http://localhost:8080/api/";
 class CommentService {
-  createComment(data) {
-    return axios.post(API_URL + "comment", data);
+  createComment(data, questionId) {
+    return axios.post(API_URL + `comment?questionId=${questionId}`, data);
   }
   getComments(questionId) {
     return axios.get(API_URL + `comment?questionId=${questionId}`);
